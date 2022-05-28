5c064247434a72e5e1d5731769a2a19b712ba775
Oleh <73610556+BessOleg@users.noreply.github.com>
2022-05-28 23:05:46 +0300

    interaction changes
    1. added interaction with objects
    2. made a character rotation display
    3. moved the render to a separate file
    4. put a semicolon in index.js

--- public/js/control.js
+++ public/js/control.js
@@ -1,44 +1,56 @@
-
-var trn = 0;
-function turn(){
-  trn++;
-  if(trn>3)
-      trn=0;
-}
-
-function move() {
-    switch (trn) {
-        case 0: myGamePiece.speedY = -30; // up
-            break;
-        case 2: myGamePiece.speedY = 30; // down
-            break;
-        case 1: myGamePiece.speedX = -30; // left
-            break;
-        case 3: myGamePiece.speedX = 30; // right
-            break;
+var trn = 2; // стартовое направление персонажа
+//поворот персонажа
+function turn() {
+    trn++;
+    if (trn == 1) myGamePiece.image.src = "/public/assets/karol_left.png";
+    if (trn == 2) myGamePiece.image.src = "/public/assets/karol_down.png";
+    if (trn == 3) myGamePiece.image.src = "/public/assets/karol_right.png";
+    if (trn > 3) {
+        trn = 0;
+        myGamePiece.image.src = "/public/assets/karol_up.png";
     }
 }
 
+//движение персонажа
+function move() {
+    switch (trn) {
+        case 0:
+            myGamePiece.speedY = -30 // up
+            break;
+        case 2:
+            myGamePiece.speedY = 30 // down
+            break;
+        case 1:
+            myGamePiece.speedX = -30  // left
+            break;
+        case 3:
+            myGamePiece.speedX = 30  // right
+            break;
+    }
+
+}
+
+//остановка персонажа
 function clearmove() {
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
 }
 
+// функцыя поднятия и вышвыривания монетки
 function token() {
-    var x = myGamePiece.x +( myGamePiece.width/2)
-    var y = myGamePiece.y + (myGamePiece.height/2)
-    if(coin.height == 0)
-    {
+    var x = myGamePiece.x + (myGamePiece.width / 2)
+    var y = myGamePiece.y + (myGamePiece.height / 2)
+
+    if (coin.height == 0) {
         coin.x = myGamePiece.x;
         coin.y = myGamePiece.y;
         coin.height = 30;
         coin.width = 30;
 
-    }
-    else if( x >= coin.x && x  <= coin.x + coin.width && y >= coin.y && y <= coin.y + coin.height){
-
+    } else if (x >= coin.x && x <= coin.x + coin.width && y >= coin.y && y <= coin.y + coin.height) {
+        trn = 2;
+        myGamePiece.image.src = "/public/assets/karol.png";
         coin.height = 0;
         coin.width = 0;
     }
-    console.log("мелочь а приятно)")
 }
--- public/js/entity.js
+++ public/js/entity.js
@@ -1,4 +1,4 @@
- function component(width, height, color, x, y,type) {
+function component(width, height, color, x, y, type) {
     this.type = type;
     this.width = width;
     this.height = height;
@@ -6,74 +6,98 @@
     this.speedY = 0;
     this.x = x;
     this.y = y;
+    if (this.type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
 
-    this.update = function() {
+    this.update = function () {
         ctx = myGameArea.context;
-        ctx.globalAlpha = 1;
-        if(this.type == "text"){
+
+        ctx.globalAlpha = 1; // прозрачность
+
+        if (this.type == "text") {
+            ctx.globalAlpha = 0.3;
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = color;
             ctx.fillText(this.text, this.x, this.y);
         }
-        if(this.type == "cube") {
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        if (this.type == "cube") {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
         }
-        if(this.type == "coin") {
-            this.image = new Image();
-            this.image.src = color;
-            ctx.drawImage(this.image,
-                this.x,
-                this.y,
-                this.width, this.height);
-        }
+
         if (type == "image") {
-
-            this.image = new Image();
-            this.image.src = color;
-            ctx.drawImage(this.image,
-                this.x,
-                this.y,
-                this.width, this.height);
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         }
 
     }
 
 
-    this.newPos = function() {
+    this.newPos = function () {
+        //up
+        if (bray.length > 0) {
+            if (this.speedX != 0)
+                this.x += this.crashWith();
+            if (this.speedY != 0)
+                this.y += this.crashWith();
+        } else {
+            this.x += this.speedX;
+            this.y += this.speedY;
+        }
 
-        this.x += this.speedX;
-        this.y += this.speedY;
-            if(this.x < 0) this.x = 0;
-            if(this.x+this.width > myGameArea.canvas.width) this.x = myGameArea.canvas.width - this.width;
-            if(this.y < 0) this.y = 0 ;
-            if(this.y+this.height > myGameArea.canvas.height )  this.y = myGameArea.canvas.height - this.height;
+        if (this.x < 0) this.x = 0;
+        if (this.x + this.width > myGameArea.canvas.width) this.x = myGameArea.canvas.width - this.width;
+        if (this.y < 0) this.y = 0;
+        if (this.y + this.height > myGameArea.canvas.height) this.y = myGameArea.canvas.height - this.height;
     }
-     this.crashWith = function() {
-         var myleft = this.x;
-         var myright = this.x + (this.width);
-         var mytop = this.y;
-         var mybottom = this.y + (this.height);
-         if(filles){
-             for (var i in filles){
-                 if(i!=0){
-                      //( a.y < b.y1 || a.y1 > b.y || a.x1 < b.x || a.x > b.x1 );
-                     var otherleft = filles[i].x;
-                     var otherright = filles[i].x + (filles[i].width);
-                     var othertop = filles[i].y;
-                     var otherbottom = filles[i].y + (filles[i].height);
-                     var crash = true;
-                     if((mybottom <= othertop) || (mytop >= otherbottom) || (myright <= otherleft) || (myleft >= otherright))
-                     {
-                         crash = false;
+    this.crashWith = function () {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        if (bray.length >= 0) {
+            for (var i in bray) {
+                var otherleft = bray[i].x;
+                var otherright = bray[i].x + bray[i].width;
+                var othertop = bray[i].y;
+                var otherbottom = bray[i].y + bray[i].height;
+                var ttn;
+                switch (trn) {
+                    case 0: { // up
+                        if ((myleft >= otherleft && myleft <= otherright) && (myright >= otherleft && myright <= otherright) && (mytop >= othertop && mytop <= otherbottom)) {
+                            ttn = 0;
+                        } else ttn = -30
 
-                     }
-                 }
-             }
-             return crash;
-
-         }
-     }
+                    }
+                        break;
+                    case 1: { // left
+                        if ((mytop >= othertop && mytop <= otherbottom) && (mybottom >= othertop && mybottom <= otherbottom) && (myleft >= otherleft && myleft <= otherright)) {
+                            ttn = 0;
+                        } else ttn = -30
+                    }
+                        break;
+                    case 2: { // down
+                        if ((myleft >= otherleft && myleft <= otherright) && (myright >= otherleft && myright <= otherright) && (mybottom >= othertop && mybottom <= otherbottom)) {
+                            ttn = 0;
+                        } else ttn = 30
+                    }
+                        break;
+                    case 3: { // right
+                        if ((mytop >= othertop && mytop <= otherbottom) && (mybottom >= othertop && mybottom <= otherbottom) && (myright >= otherleft && myright <= otherright)) {
+                            ttn = 0;
+                        } else ttn = 30
+                    }
+                        break;
+                }
+                if (ttn == 0) {
+                    return ttn;
+                }
+            }
+            return ttn;
+        }
+    }
 }
 
 
