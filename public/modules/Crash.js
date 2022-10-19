cb15de42941fb0d8dded9aee2a927c36b8a87cbf
Oleh <73610556+BessOleg@users.noreply.github.com>
2022-10-19 21:26:37 +0300

    move crash
    fix muve

--- public/modules/Crash.js
+++ public/modules/Crash.js
@@ -2,7 +2,7 @@
 let {myConfig} = require("./storage")
 
 
-let crashWith = () => {
+function crashWith () {
     let myleft = myConfig.myPlayr.x;
     let myright = myConfig.myPlayr.x + (myConfig.myPlayr.width);
     let mytop = myConfig.myPlayr.y;
@@ -13,7 +13,7 @@ let crashWith = () => {
             let otherright = myConfig.wallMass[i].x + myConfig.wallMass[i].width;
             let othertop = myConfig.wallMass[i].y;
             let otherbottom = myConfig.wallMass[i].y + myConfig.wallMass[i].height;
-            let currentStep = 0;
+            var currentStep = 0;
             switch (myConfig.levelTurn) {
                 case 0: { // up
                     if ((myleft >= otherleft && myleft <= otherright) && (myright >= otherleft && myright <= otherright) && (mytop >= othertop && mytop <= otherbottom)) {
