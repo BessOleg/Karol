60d84eba78b043057a74951e2160dea3d8b7d85d
Oleh <Bess.Oleg73@gmail.com>
2022-12-17 19:19:14 +0200

    pach server
    add cors

--- public/modules/inquiry.js
+++ public/modules/inquiry.js
@@ -2,17 +2,17 @@
 
 async function request(url, method = 'GET', data = null) {
     try {
-        const headers = {}
-        let body
+        const headers = {};
+        let body;
         if (data) {
-            headers['Content-Type'] = 'application/json'
+            headers['Content-Type'] = 'application/json';
             body = JSON.stringify(data)
         }
         const response = await fetch(url, {
             method,
             headers,
             body
-        })
+        });
         return await response.json()
     } catch (e) {
         console.warn('Error', e.message)
--- server.js
+++ server.js
@@ -4,26 +4,36 @@ var fs = require('fs');
 var myTime = 432000;// 5-day
 const app = express();
 const path = require('path');
+const cors = require("cors");
+
+app.use(cors({
+    origin:["*"]
+}));
+
 app.set('view engine', 'ejs');
 
 //app.set('view cache', true);
 
 //app.use(express.static('public'));// добавить кеширование
-app.use(express.static(__dirname + '/public', {maxAge: myTime}));
+app.use(express.static(__dirname + '/public',cors(), {maxAge: myTime}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 
 //console.log(__dirname);
-app.get('/', function (req, res) {
+app.get('/',cors(), function (req, res) {
     res.status(200).render('monit.ejs');
 
 });
-app.get('/file', (req, res) => {
+app.get('/file',cors(), (req, res) => {
+
     var rawdata = fs.readFileSync('./public/maps/levels.json');
     var level = JSON.parse(rawdata);
     res.status(200).send(JSON.stringify(level));
+   // console.log(JSON.stringify(level))
+    console.log(req.ip);
+
 });
 
 app.use((req, res) => {
