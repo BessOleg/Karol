27db84313b7fa293fff6126fd7b11f7b5d2ebd5b
Oleh <73610556+BessOleg@users.noreply.github.com>
2022-12-30 17:56:53 +0200

    small edits
    1. fix meter
    2. favicon replacement
    3. removal of junk

--- frontend/src/moduls/canvas/karolWindow.js
+++ frontend/src/moduls/canvas/karolWindow.js
@@ -1,25 +1,29 @@
 'use strict'
 import React, {useEffect, useMemo, useRef} from "react";
-import {tool} from "../js/storage";
+import {tool, htmlObj} from "../js/storage";
 import {myGameArea} from "../js/worldCanvas"
 
 require("./canvas.css")
 
 const KarolWindow = () => {
     let canRef = useRef()
+    let loadRef = useRef()
     useEffect(() => {
         myGameArea.canvas = tool.canvas = canRef.current;
         myGameArea.context = tool.ctx = tool.canvas.getContext("2d")
+        htmlObj.KarolLoad = loadRef.current;
     }, [])
+
     useMemo(() => {
         if (myGameArea.canvas !== null) {
             myGameArea.myWindowMap();
         }
-    }, [window.innerWidth, window.innerHeight])
+    }, [window.innerWidth, window.innerHeight]);
+
     return (
         <div>
             <canvas id="Canvas" ref={canRef}/>
-            <meter id="loading" value="0" min="0" max="100" optimum="85" low="33" high="66"/>
+            <meter id="loading" ref={loadRef} value="0" min="0" max="100" optimum="85" low="33" high="66"/>
         </div>
     );
 };
